12a746dd743d3337fe9fb6d25de71d49da344271
gabriel05675 <gabrielsantossv23@gmail.com>
2025-03-21 14:15:40 +0000

    questao01

---
+++ aula02/questao01.js
@@ -0,0 +1,12 @@
+let n1 = 5;
+let n2 = 4;
+let n3 = 3;
+let n4 = 2;
+let n5 = 1;
+
+let texto1 ="aprendi";
+let texto2="javascript";
+
+console.log(n1,",",n2,",",n3,",",n4,",",n5,"... ",texto1,texto2);
+
+
